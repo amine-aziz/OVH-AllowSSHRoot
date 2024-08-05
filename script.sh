f54f5ce92d0fa288090490ed426944ef27cc8d8f
Amine <71028463+Amine-Aziz@users.noreply.github.com>
2024-08-05 04:25:10 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ sudo service sshd restart
 echo "Done!"
 echo "Test SSH login with user root? (Y/n)"
     read -r input
-    if [[ "$user_input" =~ ^[Yy]$ ]]; then
+    if [[ "$input" =~ ^[Yy]$ ]]; then
         ssh root@127.0.0.1 'echo "It work!" && sleep 1 && exit'
     fi
     else
